f251fafcf57cef0fa228d0e3a1d435acf4054f76
lixinyi <1587760659@qq.com>
2024-04-26 10:20:39 +0800

    新增员工

--- sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
+++ sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
@@ -3,8 +3,12 @@ package com.sky.config;
 import com.sky.interceptor.JwtTokenAdminInterceptor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
@@ -15,16 +19,20 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
+import java.util.List;
+
 /**
  * 配置类，注册web层相关组件
  */
 @Configuration
+
+@ConfigurationProperties(prefix = "cors")
 @Slf4j
 public class WebMvcConfiguration extends WebMvcConfigurationSupport {
 
     @Autowired
     private JwtTokenAdminInterceptor jwtTokenAdminInterceptor;
-
+    protected List<String> originUrl;
     /**
      * 注册自定义拦截器
      *
@@ -36,6 +44,28 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
                 .addPathPatterns("/admin/**")
                 .excludePathPatterns("/admin/employee/login");
     }
+
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource configurationSource = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration configuration = new CorsConfiguration();
+
+            /*是否允许请求带有验证信息*/
+            configuration.setAllowCredentials(true);
+            /*允许访问的客户端域名*/
+            configuration.setAllowedOrigins(originUrl);
+
+        /*允许服务端访问的客户端请求头*/
+        configuration.addAllowedHeader("*");
+        /*允许访问的方法名,GET POST等*/
+        configuration.addAllowedMethod("*");
+        configuration.setMaxAge(3600L);
+        configurationSource.registerCorsConfiguration("/**", configuration);
+        return new CorsFilter(configurationSource);
+    }
+
+
 
     /**
      * 通过knife4j生成接口文档
@@ -64,5 +94,14 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
     protected void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/doc.html").addResourceLocations("classpath:/META-INF/resources/");
         registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+
+    public List<String> getOriginUrl() {
+        return originUrl;
+    }
+
+    public void setOriginUrl(List<String> originUrl) {
+        this.originUrl = originUrl;
     }
 }
